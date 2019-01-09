c37973e063c9fb530e65aab7916c329a3ca7ccb7
Ivo Gonçalves <kaly.imsg@gmail.com>
2019-01-09 22:43:10 +0100

    Bug fix add to head and implemented add to tail

--- LinkedList/LinkedList.js
+++ LinkedList/LinkedList.js
@@ -4,32 +4,57 @@ export default class LinkedList {
     this.length = 0;
   }
 
-
-  addItemToHead(value) {
-      const newNode = {
-        value:value,
-        next: this.head
-      };
+  addHead(value) {
+    const newNode = {
+      value:value,
+      next: this.head
+    };
 
       this.head = newNode;
       this.length++;
   }
 
   removeHead() {
+    if(this.length === 0) {
+      return undefined;
+    }
+    const value = this.head.value;
     const newHead = this.head.next;
     this.head = newHead;
     this.length--;
+    return value;
   }
 
-  addItemToTail(value) {
-
+  addTail(value) {
+    if(this.head === null) {
+      this.head.value = value;
+      this.head.next = null;
+    } else {
+      let isTail = false;
+      let currNode = this.head;
+      let nextNode = this.head.next;
+      while(!isTail) {
+        if(nextNode.next === null) {
+          const newNode = {
+            value: value,
+            next: null
+          };
+          nextNode.next = newNode;
+          isTail = true;
+        } else {
+          currNode = nextNode;
+          nextNode = nextNode.next;
+        }
+      }
+    }
+    this.length++;
   }
 
   removeTail() {
 
   }
 
-  findItem(value) {
+  find(value) {
 
   }
 
