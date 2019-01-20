ed3bf560ca3d349d50bdc935fc66b9237f757fcb
Ivo Gonçalves <kaly.imsg@gmail.com>
2019-01-20 01:28:49 +0100

    More formatting

--- BinaryHeap/MinHeap.js
+++ BinaryHeap/MinHeap.js
@@ -1,22 +1,22 @@
 export default class MinHeap {
-  constructor(){
+  constructor() {
     this.heap = [];
   }
 
-  insert(element){
+  insert(element) {
     this.heap.push(element);
     if(this.heap.length > 1) {
       let isElementInPosition = false;
       let currentIndex = this._getLastElementIndex();
       let parentIndex = this._getParentIndex(currentIndex);
       do {
-        if(this.heap[parentIndex] > this.heap[currentIndex]){
+        if(this.heap[parentIndex] > this.heap[currentIndex]) {
             //swap elements
             let parentValue = this.heap[parentIndex];
             let currentValue = this.heap[currentIndex];
             this.heap[parentIndex] = currentValue;
             this.heap[currentIndex] = parentValue;
-            if(parentIndex === 0){
+            if(parentIndex === 0) {
               isElementInPosition = true;
             } else {
               currentIndex = parentIndex;
@@ -33,15 +33,15 @@ export default class MinHeap {
     return (elementIndex-1)/2;
   }
 
-  _getLeftChildIndex(elementIndex){
+  _getLeftChildIndex(elementIndex) {
     return (2*elementIndex)+1;
   }
 
-  _getRightChildIndex(elementIndex){
+  _getRightChildIndex(elementIndex) {
     return (2*elementIndex)+2;
   }
 
-  _getLastElementIndex(){
+  _getLastElementIndex() {
     return this.heap.length-1;
   }
 }
